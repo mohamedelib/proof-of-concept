88ef481206ccc39c7008833dc314cc1efc94a8fa
Mohamed <mohamedelibrahymy374@gmail.com>
2026-06-02 14:27:07 +0200

    #3 Server js opgezet

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
-console.log('Hier komt je server voor Sprint 12.')
+// Importeer het npm package Express (uit de door npm aangemaakte node_modules map)
+// Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
+import express from "express";
+
+//  Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
+import { Liquid } from "liquidjs";
+
+// Maak een nieuwe Express applicatie aan, waarin we de server configureren
+const app = express();
+
+// Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
+// Bestanden in deze map kunnen dus door de browser gebruikt worden
+app.use(express.static("public"));
+
+// Stel Liquid in als 'view engine'
+const engine = new Liquid();
+app.engine("liquid", engine.express());
+app.set("views", "./views");
+
+// Zorg dat werken met request data makkelijker wordt
+app.use(express.urlencoded({ extended: true }));
+
+// GET routes: 
+app.get("/", async function (request, response) {
+  response.render("index.liquid");
+});
+
+// POST routes: /
+app.post("/", async function (request, response) {
+  response.redirect(303, "/");
+});
+
+// Start de server
+app.set("port", process.env.PORT || 8000);
+app.listen(app.get("port"), function () {
+  console.log(`Application started on http://localhost:${app.get("port")}`);
+});
