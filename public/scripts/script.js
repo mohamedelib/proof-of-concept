3ba99dc90714fc99932581f2b905f7c780b9a451
Mohamed <mohamedelibrahymy374@gmail.com>
2026-06-13 15:28:09 +0200

    #7 #5 UI State loading toegevoegd bij submit

---
+++ public/scripts/script.js
@@ -0,0 +1,12 @@
+document.querySelector(".login-form").addEventListener("submit", function () {
+  const input = this.querySelector('input[type="submit"]');
+  input.disabled = true;
+
+  let dots = 0;
+  input.value = "Inloggen";
+
+  setInterval(() => {
+    dots = (dots + 1) % 4;
+    input.value = "Inloggen" + ".".repeat(dots);
+  }, 400);
+});
