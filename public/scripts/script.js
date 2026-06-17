ff1e299a66a7c4578d77d369e2b4ee9e6deb5a13
Ikkezwam <mohamedelibrahymy374@gmail.com>
2026-06-17 21:17:27 +0200

    #3  JS fix

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,14 +1,17 @@
+const form = document.querySelector(".login-form"); 
 
+if (form) {
+  form.addEventListener("submit", function () {
+    const input = this.querySelector('input[type="submit"]');
+    input.disabled = true;
 
-document.querySelector(".login-form").addEventListener("submit", function () {
-  const input = this.querySelector('input[type="submit"]');
-  input.disabled = true;
+    let dots = 0;
+    input.value = "Inloggen";
 
-  let dots = 0;
-  input.value = "Inloggen";
+    setInterval(() => {
+      dots = (dots + 1) % 4;
+      input.value = "Inloggen" + ".".repeat(dots);
+    }, 400);
+  });
+}
 
-  setInterval(() => {
-    dots = (dots + 1) % 4;
-    input.value = "Inloggen" + ".".repeat(dots);
-  }, 400);
-});
