60f362447c5020a2dc2558c42d099ce7e8724b10
Ikkezwam <mohamedelibrahymy374@gmail.com>
2026-06-20 12:16:00 +0200

    Comments

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,15 +3,15 @@ const form = document.querySelector(".login-form");
 if (form) {
   form.addEventListener("submit", function () {
     const input = this.querySelector('input[type="submit"]');
-    input.disabled = true;
+     input.disabled = true; // schakelt input uit 
 
     let dots = 0;
-    input.value = "Inloggen";
+    input.value = "Inloggen"; // tekst wordt gezet op inloggen.
 
     setInterval(() => {
-      dots = (dots + 1) % 4;
+      dots = (dots + 1) % 4; //zorgt dat het van 3 weer naar 0 gaat
       input.value = "Inloggen" + ".".repeat(dots);
-    }, 400);
+    }, 400); // Herhaald elke 400ms een functie telt dots van 0 tm 3
   });
 }
 
